d9f658af51ab1d34549ec6e35c779a0f8cdfd8ca
Charles <simardcharlesantoine@gmail.com>
2026-09-25 17:54:48 -0400

    Feat : ajout shadcn

--- app/ui/navbar-acceuil.tsx
+++ app/ui/navbar-acceuil.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+"use client"
+import * as React from "react"
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
+
 
 const NavbarAcceuil = () => {
     return (
-        <div>
-                
+        <div className="">
+            <nav className="bg-primary p-10 bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+                <a
+                    href="#"
+                    className={buttonVariants({ variant:"default"})}
+                >
+                    Se connecter
+                </a>
+                <a
+                    href="#"
+                    className={buttonVariants({ variant:"default"})}
+                >
+                    Se connecter
+                </a>
+            </nav>
+
         </div>
     )
 }
